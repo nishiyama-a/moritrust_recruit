00b1e5d35005348c0ffad63cca63a057fc828247
nishiyama-a <nishiyama@queserser.co.jp>
2021-09-10 16:43:15 +0900

    requirement

--- recruit/requirement.php
+++ recruit/requirement.php
@@ -22,123 +22,122 @@
             </div>
         </div>
 
-        <main class="l-contents" role="main" id="recruit">
-            <section class="l-section read-area">
+        <main class="l-contents" role="main" id="requirement">
+            <section class="l-section">
                 <div class="sc-inr-s">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                        <ul class="clm-2">
-                            <li>
-                                <div class="clm-box h-link">
-                                    <h2 class="head-4">1.不動産事業</h2>
-                                    <ul class="list-ind">
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                                    <h2 class="head-4">2.不動産事業</h2>
-                                    <ul class="list-ind">
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                                    <h2 class="head-4">3.投資事業</h2>
-                                    <ul class="list-ind">
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                                    <h2 class="head-4">4.コーポレート事業</h2>
-                                    <ul class="list-ind">
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                                    <p>総合職（〜〜）</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                                    <p>総合職（〜〜）</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                                    <p>総合職（〜〜）</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                                    <p>総合職（〜〜）</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                                    <p>総合職（〜〜）</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                                    <p>総合職（〜〜）</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                                    <p>総合職（〜〜）</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                        <ul class="list-ind">
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                        <li>ダミーテキスト</li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                                    <p>総合職（〜〜）</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="clm-box h-link">
-                        <h2 class="head-3">見出し03見出し03</h2>
-                                    <p>総合職（〜〜）</p>
-                                </div>
-                            </li>
-                        </ul>
-                        <h2 class="head-3">見出し03見出し03</h2>
-                        <p>あああああ</p>
+                    <h2 class="head-3">職務内容</h2>
+                    <ul class="clm-2">
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-4 mb40">1.不動産事業</h2>
+                                <ul class="list-ind">
+                                    <li>都市開発の企画・計画立案</li>
+                                    <li>建築設計、設備設計</li>
+                                    <li>テナントリーシング</li>
+                                    <li>オフィス等の運営・管理企画</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-4">2.ホテル&リゾート事業</h2>
+                                <ul class="list-ind">
+                                    <li>企画および開発、誘致</li>
+                                    <li>経営、運営、投資</li>
+                                    <li>グループホテル群のトータルマネジメント</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-4">3.投資事業</h2>
+                                <ul class="list-ind">
+                                    <li>M&Aを含めた他企業との資本・業務提携</li>
+                                    <li>グループ会社の経営管理</li>
+                                    <li>自己資金やファンドによる投資全般</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-4">4.コーポレート事業</h2>
+                                <ul class="list-ind">
+                                    <li>財務、経理</li>
+                                    <li>総務、法務</li>
+                                    <li>広報</li>
+                                    <li>人事 など</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">職種</h2>
+                                <p>総合職（四大卒以上）</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">勤務地</h2>
+                                <p>東京、大阪、仙台</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">勤務時間</h2>
+                                <p>9:00～17:45<br>フレックスタイム制度、勤務間インターバル制度、<br>テレワーク制度等導入（部署により異なります）</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">休日・休暇</h2>
+                                <p>土曜・日曜・祝日・年末年始休暇、<br>有給休暇（10日～20日）、特別休暇（慶弔等）</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">初任給</h2>
+                                <p><span>大卒</span>： 月給255,000円<br><span>大学院卒</span>： 月給270,000円</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">昇給・賞与</h2>
+                                <p>昇給 年1回<br>賞与 年2回</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">福利厚生</h2>
+                                <p>各種社保完備（東京不動産業健康保険組合加入）、企業年金制度、財形貯蓄、保養所（修善寺、琵琶湖、強羅、伊東、那須、蔵王、山中湖、白馬八方、中軽井沢、南紀白浜）、親睦会
+                                </p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">教育研修</h2>
+                                <ul class="list-ind">
+                                    <li>導入研修</li>
+                                    <li>事業研修（不動産事業研修、ホテル&リゾート事業研修、投資事業研修、財務研修、法務研修、ホテル実習、ビル実習）</li>
+                                    <li>事例演習（オフィス、ホテル、店舗などの用途を組み込んだ都市開発プロジェクトのシミュレーション）</li>
+                                    <li>ビル経営管理士・不動産証券化マスター・宅建・簿記などの資格取得</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">募集学部・学科</h2>
+                                <p>全学部<br>全学科</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="clm-box h-link">
+                                <h2 class="head-3">採用予定</h2>
+                                <p>2021年4月（参考人数）<br>文系 6名<br>理系 9名</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <h2 class="head-3">応募資格</h2>
+                    2023年3月31日迄に大学・大学院を卒業・修了、もしくは卒業・修了見込みで、職歴のない方</p>
                 </div>
             </section>
 
