6290a043981675cd6f5ff3a23d8c7a63a56fcf21
nishiyama-a <nishiyama@queserser.co.jp>
2021-09-29 15:32:17 +0900

    ディレクトリ移動

--- recruit/sinsotsu/assets/inc/header.php
+++ recruit/sinsotsu/assets/inc/header.php
@@ -1,5 +1,5 @@
 <header class="header" role="banner">
-    <p class="header-logo"><a href="/"><img class="img-h" src="/assets/img/cmn/logo.svg" alt="森トラスト"></a></p>
+    <p class="header-logo"><a href="/"><img class="img-h" src="/recruit/sinsotsu/assets/img/cmn/logo.svg" alt="森トラスト"></a></p>
     <div class="header-main">
         <div class="entry">
             <p class="soon"><span class="note">coming soon</span><br>ENTRY</p>
@@ -11,7 +11,7 @@
         <section class="hamb-menu">
             <div class="sc-inr hum_wrap">
                 <div class="hum_box">
-                    <div class="hum_logo"><a href="/"><img src="/assets/img/cmn/logo_w.svg" alt="森トラスト"></a></div>
+                    <div class="hum_logo"><a href="/"><img src="/recruit/sinsotsu/assets/img/cmn/logo_w.svg" alt="森トラスト"></a></div>
                     <ul class="hum_contents">
                         <li class="contents_item">
                             <p class="soon">ENTRY <br class="disp-sp"><span class="note">coming soon</span></p>
@@ -31,81 +31,81 @@
                     <div class="hum_link">
                         <h4 class="hum_ttl">PICK UP PROJECT</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/pickup/tokyoworldgate.php">東京ワールドゲート</a></li>
-                            <li class="link"><a href="/pickup/hotel.php">3つのホテル</a></li>
-                            <li class="link"><a href="/pickup/innovation.php">イノベーション事業</a></li>
-                            <li class="link"><a href="/pickup/wellness.php">ウェルネス事業</a></li>
-                            <li class="link"><a href="/pickup/restaurant.php">カフェ・レストラン事業</a></li>
-                            <li class="link"><a href="/pickup/worcation.php">ワーケーション事業</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/pickup/tokyoworldgate.php">東京ワールドゲート</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/pickup/hotel.php">3つのホテル</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/pickup/innovation.php">イノベーション事業</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/pickup/wellness.php">ウェルネス事業</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/pickup/restaurant.php">カフェ・レストラン事業</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/pickup/worcation.php">ワーケーション事業</a></li>
                         </ul>
                     </div>
                     <div class="hum_link">
                         <h4 class="hum_ttl">INTERVIEW</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/interview/1.php">用地取得</a></li>
-                            <li class="link"><a href="/interview/2.php">不動産開発</a></li>
-                            <li class="link"><a href="/interview/3.php">ホテル企画開発</a></li>
-                            <li class="link"><a href="/interview/4.php">設計・施工監理</a></li>
-                            <li class="link"><a href="/interview/5.php">営業</a></li>
-                            <li class="link"><a href="/interview/6.php">管理</a></li>
-                            <li class="link"><a href="/interview/7.php">イノベーション投資</a></li>
-                            <li class="link"><a href="/interview/8.php">海外投資</a></li>
-                            <li class="link"><a href="/interview/9.php">広報</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/interview/1.php">用地取得</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/interview/2.php">不動産開発</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/interview/3.php">ホテル企画開発</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/interview/4.php">設計・施工監理</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/interview/5.php">営業</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/interview/6.php">管理</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/interview/7.php">イノベーション投資</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/interview/8.php">海外投資</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/interview/9.php">広報</a></li>
                         </ul>
                     </div>
                     <div class="hum_link">
                         <h4 class="hum_ttl">CROSS TALK</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/crosstalk/1.php">女性社員座談会</a></li>
-                            <li class="link"><a href="/crosstalk/2.php">OJT×新入社員対談</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/crosstalk/1.php">女性社員座談会</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/crosstalk/2.php">OJT×新入社員対談</a></li>
                         </ul>
                     </div>
                     <div class="hum_link">
                         <h4 class="hum_ttl">CEO TALK</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/ceo/">社長が語る森トラスト</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/ceo/">社長が語る森トラスト</a></li>
                         </ul>
                     </div>
                     <div class="hum_link">
                         <h4 class="hum_ttl">HUMAN <br class="disp-pc">RESOURCES</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/hr/mttalk.php">MT TALK</a></li>
-                            <li class="link"><a href="/hr/academy.php">MT ACADEMY</a></li>
-                            <li class="link"><a href="/hr/training.php">新人研修制度</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/hr/mttalk.php">MT TALK</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/hr/academy.php">MT ACADEMY</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/hr/training.php">新人研修制度</a></li>
                         </ul>
                     </div>
                     <div class="hum_link">
                         <h4 class="hum_ttl">FRESHMAN</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/freshman/">2021年入社<br>新入社員アンケート</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/freshman/">2021年入社<br>新入社員アンケート</a></li>
                         </ul>
                     </div>
                     <div class="hum_link">
                         <h4 class="hum_ttl">FIGURES</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/figures/">数字で見る森トラスト</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/figures/">数字で見る森トラスト</a></li>
                         </ul>
                     </div>
                     <div class="hum_link">
                         <h4 class="hum_ttl">OUR BUSINESS</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/ourbusiness/realestate.php">不動産事業</a></li>
-                            <li class="link"><a href="/ourbusiness/hotel.php">ホテル&リゾート事業</a></li>
-                            <li class="link"><a href="/ourbusiness/investment.php">投資事業</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/ourbusiness/realestate.php">不動産事業</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/ourbusiness/hotel.php">ホテル&リゾート事業</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/ourbusiness/investment.php">投資事業</a></li>
                         </ul>
                     </div>
                     <div class="hum_link">
                         <h4 class="hum_ttl">RECRUIT</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/recruit/message.php">採用担当から<br class="disp-sp">メッセージ</a></li>
-                            <li class="link"><a href="/recruit/requirement.php">募集要項</a></li>
-                            <li class="link"><a href="/recruit/steps.php">選考ステップ</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/recruit/message.php">採用担当から<br class="disp-sp">メッセージ</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/recruit/requirement.php">募集要項</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/recruit/steps.php">選考ステップ</a></li>
                         </ul>
                     </div>
                     <div class="hum_link">
                         <h4 class="hum_ttl">CORPORATE</h4>
                         <ul class="hum_links">
-                            <li class="link"><a href="/corporate/about.php">会社概要</a></li>
+                            <li class="link"><a href="/recruit/sinsotsu/corporate/about.php">会社概要</a></li>
                             <li class="link"><a href="https://www.mori-trust.co.jp/english/pdf/profile2021.pdf"
                                     target="_blank">パンフレット</a></li>
                         </ul>
