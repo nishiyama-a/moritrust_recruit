cb2bef71e0881eef142247b03898d770f915a4de
nishiyama-a <nishiyama@queserser.co.jp>
2021-10-05 17:15:11 +0900

    森トラスト株式会社　に表記修正

--- recruit/sinsotsu/assets/inc/head.php
+++ recruit/sinsotsu/assets/inc/head.php
@@ -1,6 +1,6 @@
 <?php
 if (empty($pageInfo['pagetitle'])) {
-    $title = "森トラスト新卒採用サイト｜株式会社森トラスト";
+    $title = "森トラスト新卒採用サイト｜森トラスト株式会社";
   } else {
     $title = $pageInfo['pagetitle'];
   }
@@ -10,17 +10,17 @@ if (empty($pageInfo['pagetitle'])) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="format-detection" content="telephone=no">
 <title><?php echo $title; ?></title>
-<meta content="「創るのは、街だけじゃない。」をコンセプトにリクルートを行う、株式会社森トラストの新卒採用サイト。" name="description">
+<meta content="「創るのは、街だけじゃない。」をコンセプトにリクルートを行う、森トラスト株式会社の新卒採用サイト。" name="description">
 <meta content="森トラスト,採用,新卒,リクルート,人事" name="keywords">
-<meta content="森トラスト新卒採用サイト｜株式会社森トラスト" property="og:title">
-<meta content="「創るのは、街だけじゃない。」をコンセプトにリクルートを行う、株式会社森トラストの新卒採用サイト。" property="og:description">
+<meta content="森トラスト新卒採用サイト｜森トラスト株式会社" property="og:title">
+<meta content="「創るのは、街だけじゃない。」をコンセプトにリクルートを行う、森トラスト株式会社の新卒採用サイト。" property="og:description">
 <meta content="http://recruit.mori-trust.co.jp/recruit/sinsotsu/assets/img/cmn/og.jpg" property="og:image">
 <meta content="http://recruit.mori-trust.co.jp/recruit/sinsotsu/" property="og:url">
 <meta content="website" property="og:type">
 <meta content="ja_JP" property="og:locale">
 <meta content="summary_large_image" property="twitter:card">
-<meta content="森トラスト新卒採用サイト｜株式会社森トラスト" property="twitter:title">
-<meta content="「創るのは、街だけじゃない。」をコンセプトにリクルートを行う、株式会社森トラストの新卒採用サイト。" property="twitter:description">
+<meta content="森トラスト新卒採用サイト｜森トラスト株式会社" property="twitter:title">
+<meta content="「創るのは、街だけじゃない。」をコンセプトにリクルートを行う、森トラスト株式会社の新卒採用サイト。" property="twitter:description">
 <meta content="http://recruit.mori-trust.co.jp/recruit/sinsotsu/assets/img/cmn/og.jpg" property="twitter:image:src">
 <link rel="icon" href="/recruit/sinsotsu/assets/img/cmn/favicon.ico">
 <link rel="apple-touch-icon" sizes="144x144" href="/recruit/sinsotsu/assets/img/cmn/apple-touch-icon-144x144.png" />
